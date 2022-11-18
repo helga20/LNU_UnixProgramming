cc47bca4c9c0c98839831c62cb02bd84221b13dd
Olia Kravets <94845931+helga20@users.noreply.github.com>
2022-11-18 17:27:48 +0200

    Add files via upload

---
+++ Practice/Task6 java 1 console/02/test2.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+from=$(mktemp -d)
+to=$(mktemp -d)
+
+mkdir $from/subdir 
+
+touch $from/1.txc
+touch $from/2.txt
+touch $from/3.c
+touch $from/subdir/4.c
+ 
+java task2 $from $to *.c
+
+if [[ ! -e $to/1.txc && ! -e $to/2.txt && -e $to/3.c && -e $to/4.c && -e $from/1.txc && -e $from/2.txt && -e $from/3.c && -e $from/subdir/4.c ]]; then
+        echo "test passed"
+else
+        echo "test failed"
+fi
+rm -r $from
+rm -r $to
+exit 0
+
