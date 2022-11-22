b35882a68fdb75afaf3acb6f4ddf89b6e5dc62fc
Olia Kravets <94845931+helga20@users.noreply.github.com>
2022-11-22 13:19:48 +0200

    Add files via upload

---
+++ Practice/Task2 gcc/37/test37.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+tdir=$(mktemp -d)
+touch $tdir/test.html
+
+cat > test.html << EOF
+<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Hello world</h1>
+    <h2>world</h2>
+  </body>
+</html>
+EOF
+
+output=$(./task37 test.html) 
+string="world : 2
+hello : 1"
+
+if [[ ( "$output" = "$string" ) ]]; then
+        echo "Test passed"
+else
+        echo "Test failed"
+fi
+rm -r $tdir
+        exit 0
+
+
